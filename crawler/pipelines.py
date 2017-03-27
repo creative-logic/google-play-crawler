18f7f75ca5f6be85910caf4ef5f2f3c686bbbcfb
odmarkj <josh@isell.com>
2017-03-26 19:40:12 -0700

    Adding sqs pipeline for storing data, adding scalable settings

--- crawler/pipelines.py
+++ crawler/pipelines.py
@@ -1,6 +1,46 @@
 from twisted.enterprise import adbapi
 import datetime
 import MySQLdb.cursors
+import boto.sqs
+from boto.sqs.message import RawMessage
+import json
+
+conf = {
+	"sqs-access-key": "",
+	"sqs-secret-key": "",
+	"sqs-queue-name": "",
+	"sqs-region": "us-east-1",
+	"sqs-path": "sqssend"
+}
+
+class SQSStorePipeline(object):
+
+    def __init__(self):
+	    self.sqs = conn = boto.sqs.connect_to_region(
+	        conf.get('sqs-region'),
+	        aws_access_key_id = conf.get('sqs-access-key'),
+	        aws_secret_access_key = conf.get('sqs-secret-key')
+		)
+		self.queue = self.sqs.get_queue(conf.get('end-queue'))
+
+    def process_item(self, item, spider):
+        # run db query in thread pool
+        print 'process item----------------------'
+        m = RawMessage()
+		m.set_body(json.dumps(item))
+		retval = self.queue.write(m)
+		print 'added message, got retval: %s' % retval
+
+        return item
+
+    def safeValue(self,value):
+        if value == [] :
+            return ''
+        else :
+            return value[0].encode("utf-8")
+
+    def handle_error(self, e):
+        print e
 
 class SQLStorePipeline(object):
 
--- crawler/settings.py
+++ crawler/settings.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # Scrapy settings for crawler project
 #
 # For simplicity, this file contains only the most important settings by
@@ -11,8 +13,22 @@ BOT_VERSION = '1.0'
 
 SPIDER_MODULES = ['crawler.spiders']
 NEWSPIDER_MODULE = 'crawler.spiders'
-USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:45.0) Gecko/20100101 Firefox/45.0'
+CONCURRENT_REQUESTS_PER_DOMAIN = 100
 
 ITEM_PIPELINES = [
-    'crawler.pipelines.SQLStorePipeline',
+    'crawler.pipelines.SQSStorePipeline',
 ]
+
+DOWNLOAD_DELAY = 2
+REACTOR_THREADPOOL_MAXSIZE = 20
+LOG_LEVEL = 'INFO'
+COOKIES_ENABLED = False
+##RETRY_ENABLED = False
+DOWNLOAD_TIMEOUT = 60
+##REDIRECT_ENABLED = False
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 1
+
+if _LOCAL_DEV_ENVIRONMENT:
+    HTTPCACHE_ENABLED=False
